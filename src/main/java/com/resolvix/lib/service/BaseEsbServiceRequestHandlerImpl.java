8740dd559664f4d07ea6d38ffcd1e59f3d126289
Roger Bisson <rwbisson@resolvix.com>
2019-05-11 17:40:38 +0100

    Added provisional support for SOAP faults

--- src/main/java/com/resolvix/lib/service/BaseEsbServiceRequestHandlerImpl.java
+++ src/main/java/com/resolvix/lib/service/BaseEsbServiceRequestHandlerImpl.java
@@ -2,16 +2,18 @@ package com.resolvix.lib.service;
 
 import com.google.common.base.Preconditions;
 import com.resolvix.lib.service.api.ServiceException;
+import com.resolvix.lib.service.api.ServiceFault;
 
 /**
  * Base implementation of an ESB -compatible service request handler.
  *
  * @param <Q> the request type
  * @param <R> the response type
+ * @param <F> the fault type
  * @param <C> the context type
  */
-public abstract class BaseEsbServiceRequestHandlerImpl<Q, R, C>
-    extends BaseServiceRequestHandlerImpl<Q, R, C>
+public abstract class BaseEsbServiceRequestHandlerImpl<Q, R, F extends ServiceFault, C>
+    extends BaseServiceRequestHandlerImpl<Q, R, F, C>
 {
 
     protected BaseEsbServiceRequestHandlerImpl() {
@@ -20,7 +22,7 @@ public abstract class BaseEsbServiceRequestHandlerImpl<Q, R, C>
 
     @Override
     protected void validate(C c)
-        throws ServiceException
+        throws ServiceException, ServiceFault
     {
         Preconditions.checkArgument(c != null);
     }
